37479035aa3f947c08657c863c49a1bf971c1e25
bo <bcainy@gmail.com>
2016-08-28 03:45:13 -0400

    v1.1

--- explorer/views.py
+++ explorer/views.py
@@ -1,15 +1,34 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 import os
+import logging
 # Create your views here.
 
 def root(req):
-    return render(req,'index.html')
-
-def getfiles(req):
     try:
-    	path = req.GET['path']
+        path = req.GET['p']
     except:
-        path =r'c:\users\bo';
-    fs = os.listdir(path)
-    return JsonResponse(fs,safe=False)
+        path = r'H:'
+    objs = os.listdir(path)
+    ds = []
+    fs = []
+    for name in objs:
+        if name.startswith('.'):
+            continue
+        _path = os.path.join(path,name)
+        o = dict(
+                name=name,
+                path=_path,
+                )
+        if os.path.isfile(_path):
+            size = os.path.getsize(_path)
+            if size>5e7:
+                #logging.critical(size)
+                fs.append(o)
+        else:
+            try:
+                os.listdir(o['path'])
+                ds.append(o)
+            except:
+                pass
+    return render(req,'index.html', {'fs':fs,'ds':ds})
