73632bb24ab98dffd173413338cd1cfecfaf4816
bo <bcainy@gmail.com>
2016-08-29 01:54:28 -0400

    v2.0

--- explorer/views.py
+++ explorer/views.py
@@ -5,30 +5,41 @@ import logging
 # Create your views here.
 
 def root(req):
+    return render(req,'index.html')
+
+def get(req):
     try:
         path = req.GET['p']
     except:
-        path = r'H:'
-    objs = os.listdir(path)
-    ds = []
-    fs = []
-    for name in objs:
+        path = 'H:'
+    names = os.listdir(path)
+    objs = [] 
+    idx = 0;
+    for name in names:
         if name.startswith('.'):
             continue
         _path = os.path.join(path,name)
+        _url = 'http://localhost:8000/?p=' + _path
         o = dict(
+                id = idx, 
                 name=name,
                 path=_path,
+                url = _url,
                 )
         if os.path.isfile(_path):
             size = os.path.getsize(_path)
             if size>5e7:
-                #logging.critical(size)
-                fs.append(o)
+                o['type']='file'
+                o['color']='red'
+                objs.append(o)
+                idx += 1
         else:
             try:
                 os.listdir(o['path'])
-                ds.append(o)
+                o['type']='folder'
+                o['color']='blue'
+                objs.append(o)
+                idx += 1
             except:
                 pass
-    return render(req,'index.html', {'fs':fs,'ds':ds})
+    return JsonResponse(objs,safe=False) 
